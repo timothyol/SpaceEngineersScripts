85006fd33f7d08c2ece2bdb063f7bd22c01ec45f
Tim <timothyol@gmail.com>
2019-04-02 21:16:47 -0500

    SE-1: Initial commit

---
+++ src/SpaceEngineersScripts/Template.cs
@@ -0,0 +1,34 @@
+﻿#region Stuff
+
+using System;
+using System.Collections.Generic;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using VRageMath;
+
+namespace SpaceEngineersScripting
+{
+    class Template
+    {
+        IMyTerminalBlock Me;
+        void Echo(string line)
+        {
+            Console.WriteLine(line);
+        }
+        IMyGridTerminalSystem GridTerminalSystem = null;
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
